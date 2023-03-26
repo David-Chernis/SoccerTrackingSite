034c53965a96cccabe0e387a1b66214d11db7214
SanHalacoglu <sanhalacoglucanada@gmail.com>
2023-03-25 18:29:55 -0700

    Added BluePrint For Populating Tables. Started Addition of teams and stats

--- data/sql.js
+++ data/sql.js
@@ -12,17 +12,65 @@ var connection = mysql.createConnection({
 //There may be connectivity issues caused by authentication (need to add IP to aws security protocol)
 
 connection.connect(async (err) => {
-    axios.get('https://api.sportmonks.com/v3/football/teams/seasons/10', {
-      params: {
-        api_token: 'IFRQw1iBMF72jh5ZjMR5OM73uCrKgx2OYkC0QMgNy2KwwFqffigo8km6ui1r',
-        includes: 'coaches;statistics.details.type',
-        filters: 'teamStatisticSeasons:10'
+  //Accessing Teams and Stats Data need to populate and also add coaches
+  axios.get('https://api.sportmonks.com/v3/football/teams/seasons/10', {
+    params: {
+      api_token: 'IFRQw1iBMF72jh5ZjMR5OM73uCrKgx2OYkC0QMgNy2KwwFqffigo8km6ui1r',
+      includes: 'coaches;statistics.details.type',
+      filters: 'teamStatisticSeasons:10'
+    }
+  })
+  .then(response => {
+      //Example of access
+      for (let i = 0; i < response.data.length; i++) {
+          teamInfo = response["data"]["data"]
+          break
       }
-    })
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.log(error);
-    });
+  })
+  .catch(error => {
+      console.log(error);
+  });
 });
+
+//To populate the Teams Table
+async function add_teams_stats(
+  team_id,
+  team_name,
+  team_code,
+  image_path,
+  founded,
+  venue_id,
+  coach,
+  goals_conceded_count,
+  goals_conceded_avg,
+  goals_scored_count,
+  goals_scored_avg,
+  cleansheets_count
+) {
+return new Promise((resolve, reject) => {
+  const sql =
+    "INSERT INTO Teams (team_id, team_name, team_code, image_path, founded, venue_id, coach, goals_conceded_count, goals_conceded_avg, goals_scored_count, goals_scored_avg, cleansheets_count) VALUES ?";
+  const values = [
+    [
+      team_id,
+      team_name,
+      team_code,
+      image_path,
+      founded,
+      venue_id,
+      coach,
+      goals_conceded_count,
+      goals_conceded_avg,
+      goals_scored_count,
+      goals_scored_avg,
+      cleansheets_count,
+    ],
+  ];
+  connection.query(sql, [values], (error, res) => {
+    if (error) throw error;
+    console.log("Team Added");
+    console.log(res)
+    resolve("Success");
+  });
+});
+}
